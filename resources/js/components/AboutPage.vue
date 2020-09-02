16127480b67c5d1703a9f2f4bd1b1566017379b3
Phone Nyo <phonenai2014@gmail.com>
2020-09-02 10:54:53 +0630

    Calling laravel route from vue component

---
+++ resources/js/components/AboutPage.vue
@@ -0,0 +1,18 @@
+<template>
+                <div class="card">
+                    <div class="card-header">About Page</div>
+
+                    <div class="card-body">
+                        Call laravel route from vue component.
+                        <a class="nav-link" :href="route('home')">Go Home</a>
+                    </div>
+                </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+                <div class="card">
+                    <div class="card-header">Home Page</div>
+
+                    <div class="card-body">
+                        Call laravel route from vue component.
+                        <a class="nav-link" :href="route('about')">Go to About</a>
+                    </div>
+                </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
